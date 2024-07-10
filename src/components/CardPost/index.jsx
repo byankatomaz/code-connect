55a3ee4f2dae0faae5c6eae7c720da0388c13405
byankatomaz <byankatomaz@gmail.com>
2024-07-10 09:34:43 -0300

    fix: update cardPost

--- src/components/CardPost/index.jsx
+++ src/components/CardPost/index.jsx
@@ -1,25 +1,25 @@
-import Image from "next/image"
-import { Avatar } from "../Avatar"
-import styles from './card.module.css'
+import Image from "next/image";
+import { Avatar } from "../Avatar";
+import styles from "./card.module.css";
+import Link from "next/link";
 
-export const CardPost = ({ post }) => {
-    return (
-        <article className={styles.article}>
-            <header className={styles.header}>
-                <figure className={styles.figure}>
-                    <Image src={post.cover}  width={438} height={133} alt="Capa post" />
-                </figure>
-            </header>
-            <section className={styles.section}>
-                <h2>{post.title}</h2>
-                <p>{post.body}</p>
-            </section>
-            <footer className={styles.footer}>
-                <Avatar 
-                imageSrc={post.author.avatar} 
-                name={post.author.username} 
-                />
-            </footer>
-        </article>
-    )
-}
+export const CardPost = ({ post, highlight }) => {
+  return (
+    <Link href={`/posts/${post.slug}`} className={styles.link}>
+      <article className={styles.article}  style={{ width: highlight === true ? 993 : 486}}>
+        <header className={styles.header}>
+          <figure className={styles.figure} style={{ height: highlight === true ? 300 : 133}}>
+            <Image src={post.cover} fill alt="Capa post" />
+          </figure>
+        </header>
+        <section className={styles.section}>
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+        </section>
+        <footer className={styles.footer}>
+          <Avatar imageSrc={post.author.avatar} name={post.author.username} />
+        </footer>
+      </article>
+    </Link>
+  );
+};
