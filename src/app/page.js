afd9a344614cad1c156a91851b45ba4828979131
byankatomaz <byankatomaz@gmail.com>
2024-07-10 09:36:30 -0300

    fix: update page

--- src/app/page.js
+++ src/app/page.js
@@ -5,25 +5,42 @@ import logger from "@/logger";
 import Link from "next/link";
 
 async function getAllPosts(page) {
-  const response = await fetch(`http://localhost:3042/posts?_page=${page}&_per_page=6`)
+  const response = await fetch(
+    `http://localhost:3042/posts?_page=${page}&_per_page=6`
+  );
 
   if (!response.ok) {
-    logger.error("teve um erro")
-    return []
+    logger.error("teve um erro");
+    return [];
   }
-  logger.info("Posts obtidos com sucesso")
-  return response.json()
+  logger.info("Posts obtidos com sucesso");
+  return response.json();
 }
 
 export default async function Home({ searchParams }) {
-  const currentPage = searchParams?.page || 1
+  const currentPage = searchParams?.page || 1;
   const { data: posts, prev, next } = await getAllPosts(currentPage);
 
   return (
-    <main className={styles.grid}>
-      {posts.map(post => <CardPost post={post} key={post.id} />)}
-      {prev && <Link href={`/?page=${prev}`}>Página anterior</Link>}
-      {next && <Link href={`/?page=${next}`}>Proxima página</Link>}
+    <main>
+      <div className={styles.grid}>
+        {posts.map((post) => (
+          <CardPost post={post} key={post.id} highlight={false} />
+        ))}
+      </div>
+
+      <div className={styles.cardLink}>
+        {prev && (
+          <Link className={styles.link} href={`/?page=${prev}`}>
+            Página anterior
+          </Link>
+        )}
+        {next && (
+          <Link className={styles.link} href={`/?page=${next}`}>
+            Proxima página
+          </Link>
+        )}
+      </div>
     </main>
   );
 }
