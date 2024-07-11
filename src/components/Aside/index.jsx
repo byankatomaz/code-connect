436dd828de875a513d6c4e1ea76b6dc563343118
byankatomaz <byankatomaz@gmail.com>
2024-07-11 17:41:41 -0300

    fix: update aside

--- src/components/Aside/index.jsx
+++ src/components/Aside/index.jsx
@@ -1,11 +1,14 @@
-import Image from 'next/image'
-import style from './aside.module.css'
-import logo from './logo.png'
+import Image from "next/image";
+import style from "./aside.module.css";
+import logo from "./logo.png";
+import Link from "next/link";
 
 export const Aside = () => {
-    return (
-        <aside className={style.aside}>
-            <Image src={logo} alt="Logo da Code Connect"/>
-        </aside>
-    )
-}
+  return (
+    <aside className={style.aside}>
+      <Link href='/'>
+        <Image src={logo} alt="Logo da Code Connect" />
+      </Link>
+    </aside>
+  );
+};
