f68460a43d588b9dd865925cbbd6bf6fea4469e6
byankatomaz <byankatomaz@gmail.com>
2024-07-11 17:42:14 -0300

    feat: new component thumbsup

---
+++ src/components/CardPost/ThumbsUpButton.jsx
@@ -0,0 +1,12 @@
+'use client'
+
+import { IconButton } from "../IconButton";
+import { ThumbsUp } from "../icons/ThumbsUp";
+import { Spinner } from "../Spinner";
+
+import { useFormStatus } from "react-dom";
+
+export const ThumbsUpButton = () => {
+  const { pending } = useFormStatus();
+  return <IconButton disable={pending}>{pending ? <Spinner /> : <ThumbsUp />}</IconButton>;
+};
