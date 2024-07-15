b1834add30d91a0712ae12f7cae56244d6a6d1aa
byankatomaz <byankatomaz@gmail.com>
2024-07-15 15:23:25 -0300

    fix: update modal reply

---
+++ src/components/ModalReply/index.jsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { useRef } from "react";
+import { IconButton } from "../IconButton";
+import { Modal } from "../Modal";
+import { Textarea } from "../Textarea";
+
+import styles from "./modalreply.module.css";
+import { SubmitButton } from "../SubmitButton";
+import { Comment } from "../Comment";
+import { postReply } from "@/actions";
+
+export const ModalReply = ({ comment, post }) => {
+  const modalRef = useRef(null);
+
+  const openModal = () => {
+    modalRef.current.openModal();
+  };
+
+  const action = postReply.bind(post, comment);
+
+  return (
+    <>
+      <Modal ref={modalRef}>
+        <form action={action}>
+          <div className={styles.body}>
+            <Comment comment={comment} />
+          </div>
+          <div className={styles.divider}/>
+          <Textarea
+            required
+            rows={8}
+            name="text"
+            placeholder="Digite aqui..."
+          />
+          <div className={styles.footer}>
+            <SubmitButton>Responder</SubmitButton>
+          </div>
+        </form>
+      </Modal>
+      <IconButton className={styles.btn} onClick={openModal}>
+        Responder
+      </IconButton>
+    </>
+  );
+};
